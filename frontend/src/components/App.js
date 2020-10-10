3924f20fb51a999371d960ff5a4431c5842298e1
horlamiedea <horlamiedea@gmail.com>
2020-10-10 01:25:11 +0100

    making a lead manager with react and django

---
+++ frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import ReactDom from 'react-dom'
+import Header from './layout/Header';
+
+class App extends Component {
+    render() {
+        return (
+            <Header />
+        )
+    }
+}
+
+ReactDom.render(<App />, document.getElementById('app'))
