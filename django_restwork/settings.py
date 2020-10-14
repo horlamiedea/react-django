2df9cfe4eb724425b48ab132337c12c464f5c4d2
horlamiedea <horlamiedea@gmail.com>
2020-10-14 22:38:01 +0100

    added a build pack to react frontend

--- django_restwork/settings.py
+++ django_restwork/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'ew5@xy981^lwm3@_cdal4((2+m95e8zx-qtips9v5_idr0exwr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['reacttdj.herokuapp.com']
+ALLOWED_HOSTS = ['reacttdj.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
